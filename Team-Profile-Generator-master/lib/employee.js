45bded7269af353ac8d8bf15d77041383667bde0
Your Name <brandon.sellam@gmail.com>
2020-04-25 23:53:50 +0200

    Work done

---
+++ Team-Profile-Generator-master/lib/employee.js
@@ -0,0 +1,25 @@
+
+class Employee{
+    
+    constructor(name, email, id){
+        this.name = name;
+        this.email = email;
+        this.id = id;
+
+    }
+    getName(){
+        return this.name;
+    };
+    getId(){
+        return this.id;
+    };
+    getRole(){
+        return "Employee";
+    };
+    getEmail(){
+        return this.email;
+    }
+
+}
+
+module.exports = Employee;
